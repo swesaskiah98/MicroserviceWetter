6360d0a30999536439d697cfbeaafb4eee6c6105
Saskia <wi18b092@technikum-wien.at>
2021-11-04 12:16:23 +0100

    Added Tests

---
+++ MicroserviceAPIWetter/Test/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroserviceAPIWetter.Model;
+using NUnit.Framework;
+
+namespace MicroserviceAPIWetter.Test {
+    [TestFixture]
+    public class Test {
+        Database database;
+        [SetUp]
+        public void SetUp () {
+            database = new Database();
+        }
+        [Test]
+        public void test1 () {
+            Assert.AreEqual(1, 1);
+        }
+        [Test]
+        public void databaseNotEmpty () {
+            List<WetterData> wetterListe = database.getData();
+            Assert.AreEqual(wetterListe[0].datum, DateTime.Today);
+        }
+
+    }
+}
