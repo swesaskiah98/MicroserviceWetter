f97f2e7b2297493fe1acf3e81a60799d8f7ab438
Saskia <wi18b092@technikum-wien.at>
2021-11-04 13:08:53 +0100

    Tests laufen

--- MicroserviceAPIWetter/Program.cs
+++ MicroserviceAPIWetter/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MicroserviceAPIWetter.Test;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,9 @@ namespace MicroserviceAPIWetter {
     public class Program {
         public static void Main (string[] args) {
             CreateHostBuilder(args).Build().Run();
+            //var t=new Test.Test();
+            //t.test1();
+            //t.databaseNotEmpty();
         }
 
         public static IHostBuilder CreateHostBuilder (string[] args) =>
